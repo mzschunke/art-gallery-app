4da469c2bf8722fe96ffa5bb8022a74a04c9de39
Moritz Zschunke <moritz_z17@web.de>
2023-04-20 14:49:38 +0200

    implented fetch function

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import useSWR from "swr";
+
+const URL = "https://example-apis.vercel.app/api/art";
+const fetcher = (URL) => fetch(URL).then((res) => res.json());
+
 export default function HomePage() {
+  const { data, error } = useSWR(URL, fetcher);
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+  console.log(data);
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Art Gallery</h1>
     </div>
   );
 }
+
+
+
+
+
